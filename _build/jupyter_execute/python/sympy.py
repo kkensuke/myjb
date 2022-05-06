4186d6e630e137090e9dc5c441fa72250d8113d7
kk564 <n4659017382@gmail.com>
2022-05-06 21:49:09 +0900

    add

--- _build/jupyter_execute/python/sympy.py
+++ _build/jupyter_execute/python/sympy.py
@@ -3,58 +3,55 @@
 
 # # Sympy
 
-# In[8]:
+# In[27]:
 
 
 from sympy import *
 import numpy as np
-import matplotlib.pyplot as plt
-
-from IPython.display import display
 
 import sympy
-sympy.init_printing() # If all you want is the best pretty printing, use the init_printing() function.
+#sympy.init_printing() # If all you want is the best pretty printing, use the init_printing() function.
 
 
-# In[9]:
+# In[17]:
 
 
 x = Symbol('x')
 y = Symbol('y')
 
 
-# In[10]:
+# In[18]:
 
 
 (x + y)**2
 
 
-# In[11]:
+# In[19]:
 
 
 f = expand((x + y)**2)
 display(f)
 
 
-# In[12]:
+# In[20]:
 
 
 f.subs({x:1, y:2})
 
 
-# In[13]:
+# In[21]:
 
 
 factor(x**2-4*x+3)
 
 
-# In[14]:
+# In[22]:
 
 
 apart(1/(x**5-1))
 
 
-# In[15]:
+# In[23]:
 
 
 a = Symbol('a') # Without real=True, a is treated as a complex number.
@@ -64,28 +61,28 @@ u = exp(a*x)*sin(b*x)
 display(u)
 
 
-# In[16]:
+# In[24]:
 
 
 int_u = integrate(u, x)
 display(int_u)
 
 
-# In[17]:
+# In[25]:
 
 
 R = diff(u, x, 2) + u + x
 display(R)
 
 
-# In[18]:
+# In[26]:
 
 
 k, N = sympy.symbols('k, N', integer = True)
 sympy.factor(sympy.summation(k, (k, 1, N) ))
 
 
-# In[19]:
+# In[15]:
 
 
 s = Symbol('s')
