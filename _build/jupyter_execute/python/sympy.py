e37c61926acc4e93d43e2b2ea6ba16dd7d60bf93
kk564 <n4659017382@gmail.com>
2022-05-06 21:52:51 +0900

    add

--- _build/jupyter_execute/python/sympy.py
+++ _build/jupyter_execute/python/sympy.py
@@ -3,55 +3,52 @@
 
 # # Sympy
 
-# In[28]:
+# In[1]:
 
 
 from sympy import *
-import numpy as np
-
-import sympy
 #sympy.init_printing()
 
 
-# In[29]:
+# In[2]:
 
 
 x = Symbol('x')
 y = Symbol('y')
 
 
-# In[30]:
+# In[3]:
 
 
 (x + y)**2
 
 
-# In[31]:
+# In[4]:
 
 
 f = expand((x + y)**2)
 display(f)
 
 
-# In[32]:
+# In[5]:
 
 
 f.subs({x:1, y:2})
 
 
-# In[33]:
+# In[6]:
 
 
 factor(x**2-4*x+3)
 
 
-# In[34]:
+# In[7]:
 
 
 apart(1/(x**5-1))
 
 
-# In[35]:
+# In[8]:
 
 
 a = Symbol('a') # Without real=True, a is treated as a complex number.
@@ -61,28 +58,28 @@ u = exp(a*x)*sin(b*x)
 display(u)
 
 
-# In[36]:
+# In[9]:
 
 
 int_u = integrate(u, x)
 display(int_u)
 
 
-# In[37]:
+# In[10]:
 
 
 R = diff(u, x, 2) + u + x
 display(R)
 
 
-# In[38]:
+# In[11]:
 
 
-k, N = sympy.symbols('k, N', integer = True)
-sympy.factor(sympy.summation(k, (k, 1, N) ))
+k, N = symbols('k, N', integer = True)
+factor(summation(k, (k, 1, N) ))
 
 
-# In[39]:
+# In[12]:
 
 
 s = Symbol('s')
