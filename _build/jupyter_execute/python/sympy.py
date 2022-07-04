5452692410b407b56016f1ef9904614f6275b83e
kkensuke <n4659017382@gmail.com>
2022-07-05 03:28:23 +0900

    add

--- _build/jupyter_execute/python/sympy.py
+++ _build/jupyter_execute/python/sympy.py
@@ -6,57 +6,71 @@
 # 
 # In this page, you see examples of sympy usage.
 
-# In[1]:
+# In[4]:
 
 
 from sympy import *
 
 
-# In[2]:
+# ## Declare symbols
+
+# In[5]:
 
 
 x = Symbol('x')
 y = Symbol('y')
 
 
-# In[3]:
+# In[6]:
 
 
 (x + y)**2
 
 
-# In[4]:
+# ## Expansion
+
+# In[7]:
 
 
 f = expand((x + y)**2)
 display(f)
 
 
-# In[5]:
+# ## Substitution
+
+# In[8]:
 
 
 f.subs({x:1, y:2})
 
 
-# In[6]:
+# ## Factorization
+
+# In[9]:
 
 
 factor(x**2-4*x+3)
 
 
-# In[7]:
+# ## Solve equations
+
+# In[10]:
 
 
 solve(x**2 - x - 1)
 
 
-# In[8]:
+# ## Partial fraction decomposition
+
+# In[11]:
 
 
 apart(1/(x**5-1))
 
 
-# In[9]:
+# ## Integrals and derivatives
+
+# In[12]:
 
 
 a = Symbol('a') # Without real=True, a is treated as a complex number.
@@ -66,28 +80,40 @@ u = exp(a*x)*sin(b*x)
 display(u)
 
 
-# In[10]:
+# In[13]:
 
 
 int_u = integrate(u, x)
 display(int_u)
 
 
-# In[11]:
+# In[14]:
 
 
 R = diff(u, x, 2) + u + x
 display(R)
 
 
-# In[12]:
+# ## Summation
+
+# In[15]:
 
 
 k, N = symbols('k, N', integer = True)
 factor(summation(k, (k, 1, N) ))
 
 
-# In[ ]:
+# ## Limits
+
+# In[18]:
+
+
+limit(sin(x)/x, x, 0)
+
+
+# ## Other Examples
+
+# In[16]:
 
 
 s = Symbol('s')
@@ -95,12 +121,12 @@ t = Symbol('t')
 
 l = (s**2 * x**3) + (t * x**2) + (3 * x) + 1
 
+display(l)
+
+
+# In[17]:
+
+
 int_l = integrate(l, (x, 0, 1))
 display(int_l)
-
-
-# In[ ]:
-
-
-limit(sin(x)/x, x, 0)
 
